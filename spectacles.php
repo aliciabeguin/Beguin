6ade241b2d17960758878dc6216240326377f3e9
Dudu13600 <boukhssibi@cy-tech.fr>
2022-05-24 10:26:37 +0200

    coucou

--- spectacles.php
+++ spectacles.php
@@ -17,37 +17,43 @@
 
 		<div class="listeSpectacle">
 			<div class="spectacleOrque">
-				<div class="titre">Spectacles des orques</div>
-				<div class="description">
-					Venez nombreux voir notre spectacle d'orques 
-				</div>
-				<div class="place_spectacle">
-					<div class="place">Place restante : 4</div>
-					<div class="tarifs">Tarifs : 6 €</div>
+				<div class='groupe'>
+					<div class="titre">Spectacles des orques</div>
+					<div class="description">
+						Venez nombreux voir notre spectacle d'orques 
+					</div>
+					<div class="place_spectacle">
+						<div class="place">Place restante : 4</div>
+						<div class="tarifs">Tarifs : 6 €</div>
+					</div>
 				</div>
 				<img src="images/spectacleOrque.webp">
 			</div>
 
 			<div class="spectacleDauphin">
-				<div class="titre">Spectacles des dauphins</div>
-				<div class="description">
-					Venez nombreux voir notre spectacle de dauphins
+				<div class='groupe'>
+					<div class="titre">Spectacles des dauphins</div>
+					<div class="description">
+						Venez nombreux voir notre spectacle de dauphins
+					</div>
+					<div class="place_spectacle">
+						<div class="place">Place restante : 4</div>
+						<div class="tarifs">Tarifs : 6 €</div>
+					</div>	
 				</div>
-				<div class="place_spectacle">
-					<div class="place">Place restante : 4</div>
-					<div class="tarifs">Tarifs : 6 €</div>
-				</div>	
-				<img src="mages/spectacleDauphin.webp">			
+				<img src="images/spectacleDauphin.jpg">			
 			</div>
 			
 			<div class="spectaclePinguins">
-				<div class="titre">Spectacles des pinguins</div>
-				<div class="description">
-					Venez nombreux voir notre spectacle de pinguins
-				</div>
-				<div class="place_spectacle">
-					<div class="place">Place restante : 4</div>
-					<div class="tarifs">Tarifs : 6 €</div>
+				<div class='groupe'>
+					<div class="titre">Spectacles des pinguins</div>
+					<div class="description">
+						Venez nombreux voir notre spectacle de pinguins
+					</div>
+					<div class="place_spectacle">
+						<div class="place">Place restante : 4</div>
+						<div class="tarifs">Tarifs : 6 €</div>
+					</div>
 				</div>
 				<img src="images/spectaclePinguins.webp">
 			</div>
